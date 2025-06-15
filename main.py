59c9589965739f2d62a3fbcf2baca9745324e54a
Matheus <66970080+matheusfarocha@users.noreply.github.com>
2025-06-15 02:25:03 -0400

    open and register links as seen, included comments.

--- main.py
+++ main.py
@@ -2,13 +2,21 @@ import os
 from dotenv import load_dotenv
 from supabase import create_client, Client
 
+# loads .env in environment  
 load_dotenv()
-
+# sets url/keys for database
 url = os.environ.get("SUPABASE_URL")
 key = os.environ.get("SUPABASE_KEY")
 
+# initializes connection to database
 supabase: Client = create_client(url, key)
 
-response = supabase.table("Main").select("*").execute()
-data = response.data
-print(data)
+# fetches table 
+table = supabase.table("Main").select("id, file_url, is_read, file_type").execute()
+
+# iterates through table, if data hasn't been seen it opens and registers as seen
+for row in table.data:
+    if row['is_read'] == False and row['file_type'] == "URL":
+        os.system(f"open {row['file_url']}")
+        supabase.table("Main").update({"is_read": True}).eq("id", row["id"]).execute()
+        os.system("sleep 1")
