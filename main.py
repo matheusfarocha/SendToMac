7a75df7b949ba449ede503c2531fe246a910e860
Matheus <66970080+matheusfarocha@users.noreply.github.com>
2025-06-15 01:22:24 -0400

    setup API requests to supabase database

---
+++ main.py
@@ -0,0 +1,14 @@
+import os
+from dotenv import load_dotenv
+from supabase import create_client, Client
+
+load_dotenv()
+
+url = os.environ.get("SUPABASE_URL")
+key = os.environ.get("SUPABASE_KEY")
+
+supabase: Client = create_client(url, key)
+
+response = supabase.table("Main").select("*").execute()
+data = response.data
+print(data)
