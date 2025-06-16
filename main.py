6b899e31b85749992e5dea0232f15a0d7761d065
Matheus <66970080+matheusfarocha@users.noreply.github.com>
2025-06-16 00:18:42 -0400

    added support for TEXT and loops every 60s

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 from dotenv import load_dotenv
 from supabase import create_client, Client
+import time
 
 # loads .env in environment  
 load_dotenv()
@@ -12,11 +13,18 @@ key = os.environ.get("SUPABASE_KEY")
 supabase: Client = create_client(url, key)
 
 # fetches table 
-table = supabase.table("Main").select("id, file_url, is_read, file_type").execute()
+while True:
+    table = supabase.table("Main").select("id, file_data, is_read, file_type").execute()
 
-# iterates through table, if data hasn't been seen it opens and registers as seen
-for row in table.data:
-    if row['is_read'] == False and row['file_type'] == "URL":
-        os.system(f"open {row['file_url']}")
-        supabase.table("Main").update({"is_read": True}).eq("id", row["id"]).execute()
-        os.system("sleep 1")
+    # iterates through table, if data hasn't been seen it opens and registers as seen 
+
+    for row in table.data:
+        if row['is_read'] == False:
+            if row['file_type'] == "URL":
+                os.system(f"open {row['file_data']}")
+            elif row['file_type'] == "TEXT":
+                os.system(f"echo {row['file_data']} > /tmp/sendtomac.txt")
+                os.system("open /tmp/sendtomac.txt")
+            supabase.table("Main").update({"is_read": True}).eq("id", row["id"]).execute()
+            time.sleep(1)
+    time.sleep(60)
